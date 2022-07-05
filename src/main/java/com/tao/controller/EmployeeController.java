48b2e8c9e0150cd2045f92311779c5c3c0db0b30
taooa <2749767329@qq.com>
2022-07-05 17:13:53 +0800

    reg

--- src/main/java/com/tao/controller/EmployeeController.java
+++ src/main/java/com/tao/controller/EmployeeController.java
@@ -1,17 +1,21 @@
 package com.tao.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.tao.common.R;
 import com.tao.pojo.Employee;
 import com.tao.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -20,10 +24,67 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
-    
+
     @PostMapping("/login")
     public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
-        return null;
+        //将页面提交的密码进行md5加密
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        //根据用户名username查询数据库
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<Employee>();
+        queryWrapper.eq(Employee::getUsername,employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+
+        //如果没有查询到i则返回失败结果
+        if(emp == null){
+            return R.error("登录失败");
+        }
+
+        //密码比对，如果不一致则返回登录失败结果
+        if(!emp.getPassword().equals(password)){
+            return R.error("登录失败");
+        }
+
+        //查看员工状态
+        if(emp.getStatus() == 0){
+            return R.error("帐号已经禁用");
+        }
+
+        //登录成功，将员工id存入session并返回登录成功结果
+        request.getSession().setAttribute("employee",emp.getId());
+        return R.success(emp);
+
+    }
+
+    //员工退出
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+
+        //清理员工session
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+
+    }
+
+    //新增员工
+    @PostMapping
+    public R<String> save(HttpServletRequest request,@RequestBody Employee employee){
+        log.info("新增员工：{}",employee.toString());
+        //设置初始密码进行加密
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //获取当前登录用户id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+
+        employeeService.save(employee);
+        return R.success("新增员工成功");
 
     }
 }
---
+++ src/main/java/com/tao/filter/LoginCheckFilter.java
@@ -0,0 +1,65 @@
+package com.tao.filter;
+
+
+import com.alibaba.fastjson.JSON;
+import com.tao.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
+public class LoginCheckFilter implements Filter {
+    public  static  final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        //获取请求uri
+        String requestURI = request.getRequestURI();
+
+        //不需要处理的路径
+        String[] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+
+        //判断是否需要处理
+        boolean check = check(urls,requestURI);
+
+        //如果不需要处理，放行
+        if(check){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //判断登录状态，如果已经登录，放行
+        if(request.getSession().getAttribute("employee")!= null){
+            return;
+        }
+
+        //没有登录，通过输出流让前端响应
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+        return;
+
+    }
+
+    public boolean check(String[] urls,String requestURI){
+        for(String url : urls) {
+            boolean match = PATH_MATCHER.match(url,requestURI);
+            if(match){
+                return true;
+            }
+        }
+        return false;
+    }
+}
